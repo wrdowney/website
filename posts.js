7d6f8d905f0bd7bf9cf6ff89dea6af7408a9f218
wrdowney <wdowney20@gmail.com>
2024-08-25 15:00:47 -0400

    render fetched posts on the DOM

--- posts.js
+++ posts.js
@@ -5,8 +5,30 @@ fetch("http://localhost:3333/feed")
         if (!response.ok) {
             throw new Error("Network response was not ok");
         }
-
-        return response.json();
+        return response.json()
     })
-    .then((data) => console.log(data))
+    .then((data) => {
+        
+        data.Channel.Items.forEach((item) => {
+            const maxPreviewLength = 300
+
+            const post = document.createElement("div");
+
+            const title = document.createElement("h3");
+            title.innerText = item.Title;
+            post.appendChild(title);
+
+            const publishDate = document.createElement("p");
+            publishDate.innerText = item.PubDate;
+            publishDate.style.font = "italic 0.8rem Courier,serif";
+            post.appendChild(publishDate);
+
+            const content = document.createElement("div");
+            content.innerHTML = (item.Content.length <= maxPreviewLength) ? item.Content : item.Content.substr(0, maxPreviewLength) + '\u2026';
+            post.appendChild(content)
+
+            document.getElementById("posts").appendChild(post);
+        });
+        console.log(data);
+    })
     .catch((error) => console.error("Fetch error: ", error));
