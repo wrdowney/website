14fa211c52209735a6518ca39e45f3588f09da51
wrdowney <wdowney20@gmail.com>
2024-08-25 13:33:38 -0400

    decoding logic for rss responses from medium

--- serv/main.go
+++ serv/main.go
@@ -4,15 +4,38 @@ import (
     "errors"
     "fmt"
     "io"
-    "io/ioutil"
     "net/http"
     "os"
     "log"
+    "encoding/xml"
 )
+
+type Item struct {
+    Title     string    `xml:"title"`
+    Link      string    `xml:"link"`
+    Guid      string    `xml:"guid"`
+    Creator   string    `xml:"creator"`
+    PubDate   string    `xml:"pubDate"`
+    Updated   string    `xml:"updated"`
+    Content   string    `xml:"encoded"`
+}
+
+type Channel struct {
+    Title string `xml:"title"`
+    Link  string `xml:"link"`
+    Desc  string `xml:"description"`
+    Items []Item `xml:"item"`
+}
+
+type Rss struct {
+    Channel Channel `xml:"channel"`
+}
+
+var rss Rss
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
     fmt.Printf("got / request\n")
-    io.WriteString(w, "This is my website!\n")
+    io.WriteString(w, rss.Channel.Items[0].Content)
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
@@ -21,22 +44,24 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
-    resp, err := http.Get("https://medium.com/@wdowney20/fee")
-
+    resp, err := http.Get("https://medium.com/@wdowney20/feed")
     if err != nil {
-        log.Fatal(err)
+        log.Fatalf("Error Get: %v\n", err)
     }
-
     defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
+    rss = Rss{}
 
+    decoder := xml.NewDecoder(resp.Body)
+    err = decoder.Decode(&rss)
     if err != nil {
-        log.Fatal(err)
+        log.Fatalf("Error Decode: %v\n", err)
+        return
     }
 
-    fmt.Println(string(body))
+    for _, item := range rss.Channel.Items {
+        log.Printf("title: %v pub: %v test: %v\n", item.Title, item.PubDate, item.Content)
+    }
 
     http.HandleFunc("/", getRoot)
     http.HandleFunc("/hello", getHello)
@@ -44,9 +69,9 @@ func main() {
     err = http.ListenAndServe(":3333", nil)
 
     if errors.Is(err, http.ErrServerClosed) {
-        fmt.Printf("server closed\n")
+        log.Print("server closed\n")
     } else if err != nil {
-        fmt.Printf("error starting server: %s\n", err)
+        log.Printf("error starting server: %s\n", err)
         os.Exit(1)
     }
 }
